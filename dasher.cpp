5f9fa8493504b5432d47a584c42bb00bce2df455
justin macklin <jmacklin308@gmail.com>
2021-08-14 19:33:43 -0500

    Cleaned up comments, removed regions, and added an array

--- dasher.cpp
+++ dasher.cpp
@@ -12,10 +12,16 @@ struct AnimData
 
 int main()
 {
+
+	//window demensions
+	int windowDemensions[2];
+	windowDemensions[0] = 1280; //screen width
+	windowDemensions[1] = 720;	//screen height
+
 	//game window
 	const int screen_width{1280};
 	const int screen_height{720};
-	InitWindow(screen_width, screen_height, "Dapper Dasher");
+	InitWindow(windowDemensions[0], screen_height, "Dapper Dasher");
 
 	//for double jump prevention
 	bool isinAir{false};
@@ -23,24 +29,29 @@ int main()
 	//acceleration due to graveity (pixels per second /per second)
 	const int rec_gravity{1500};
 
-	//Texture for player spritesheet
+	//(SCARFY) Texture for player spritesheet
 	Texture2D scarfy = LoadTexture("textures/scarfy.png");
-	//velocity for movment
+	//(SCARFY) velocity for movment
 	int rec_jump_velocity{-900}; //pixles per second
 	int rec_current_velocity{0};
 
-	AnimData scarfy_data{
-		{scarfy.width / 6, scarfy.height, 0, 0},
-		{screen_width / 8 - scarfy.width / 2, screen_height / 2 - scarfy_data.rec.height},
-		0,
-		1.0f / 12.0f,
-		0.0f};
+	//(SCARFY) scarfy animation data
+	AnimData scarfy_data;
+	scarfy_data.rec.width = scarfy.width / 6;
+	scarfy_data.rec.height = scarfy.height;
+	scarfy_data.rec.x = 0;
+	scarfy_data.rec.y = 0;
+	scarfy_data.pos.x = screen_width / 8 - scarfy_data.rec.width / 2;
+	scarfy_data.pos.y = screen_height / 2 - scarfy_data.rec.height;
+	scarfy_data.frame = 0;
+	scarfy_data.update_time = 1.0f / 12.0f;
+	scarfy_data.running_time = 0.0;
 
-	//Texture for nebulas
+	//(NEBULAS) Texture for nebulas
 	Texture2D nebula = LoadTexture("Textures/12_nebula_spritesheet.png");
-	//nebula x velocity (Pixels per second)
+	//(NEBULAS)nebula x velocity (Pixels per second)
 	int nebVel{-600};
-	//Our data for our nebulas
+	//(NEBULAS) data for our nebulas
 	AnimData neb_data{
 		{0.0f, 0.0f, nebula.width / 8.0f, nebula.height / 8.0f}, // Rectangle rec
 		{screen_width, screen_height - nebula.height / 8.0f},	 //  Vector2 pos
@@ -55,7 +66,7 @@ int main()
 		1.0f / 16.0f,												// float Update Time
 		0.0f														// float Running time
 	};
-
+	//(CORE) Game loop----------------------------------------------------------------------------------------------
 	SetTargetFPS(60);
 	while (!WindowShouldClose())
 	{
@@ -68,9 +79,7 @@ int main()
 
 		ClearBackground(RAYWHITE);
 
-#pragma region //* Gravity and player collsion
-
-		//Ground check & apply gravity----------------------------------------------------------------
+		//(SCARFY) Ground check & apply gravity----------------------------------------------------------------
 		if (scarfy_data.pos.y >= screen_height - scarfy_data.rec.height)
 		{
 			if (scarfy_data.pos.y > screen_height - scarfy_data.rec.height)
@@ -96,16 +105,12 @@ int main()
 				scarfy_data.pos.y -= amountOver;
 			}
 		}
-#pragma endregion
-
-#pragma region //*input checking
+		//---------------------------------------------------------------------------------------------------------
 		if (IsKeyPressed(KEY_SPACE) && isinAir == false)
 		{
 			rec_current_velocity += rec_jump_velocity;
 		}
-#pragma endregion
 
-#pragma region //*Update Nebulas postion and animate
 		//update nebula position
 		neb_data.pos.x += nebVel * dt;
 		if (neb_data.pos.x <= 0 - neb_data.rec.width)
@@ -144,9 +149,7 @@ int main()
 				neb_2_data.frame = 0;
 			}
 		}
-#pragma endregion
 
-#pragma region //*update movement and animate the player
 		//update player position
 		scarfy_data.pos.y += rec_current_velocity * dt;
 
@@ -168,9 +171,7 @@ int main()
 			//freeze player animation to first one when jumpint
 			scarfy_data.frame = 0;
 		}
-#pragma endregion
 
-#pragma region //********DRAWING
 		//draw nebula
 		DrawTextureRec(nebula, neb_data.rec, neb_data.pos, WHITE);
 
@@ -181,7 +182,6 @@ int main()
 		DrawTextureRec(scarfy, scarfy_data.rec, scarfy_data.pos, WHITE);
 
 		EndDrawing();
-#pragma endregion
 	}
 	CloseWindow();
 	//unload our player texture
